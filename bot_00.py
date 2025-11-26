94b4d74bf2b8b8ad9fe120a01583c9cbef25b4ae
leonshimizu <lmshimizu@gmail.com>
2025-11-26 18:18:49 +1000

    Make it multi-turn dialogue

--- bot_00.py
+++ bot_00.py
@@ -3,29 +3,32 @@ from openai import OpenAI
 
 load_dotenv()
 
-# llm = OpenAI()
+llm = OpenAI()
 
+user_input = input("Assistant: How can I help you today?\n\nUser: ")
 
-# response = llm.responses.create(
-#   model="gpt-4.1-mini",
-#   temperature=1,
-#   input=f"Respond to the following like a pirate: {user_input}"
-#   # augmenting the prompt 
-# )
-
-# print(response.output_text)
-
-user_input = input("Share anything with me and I'll translate it to French: \n")
-
-def translate_to_french(text):
-  llm = OpenAI()
+while user_input != "exit":
   response = llm.responses.create(
     model="gpt-4.1-mini",
     temperature=1,
-    input=f"Translate the following into French, and only include the translation itself with no introductory text: {text}"
+    input=user_input
   )
 
-  return response.output_text
+  print(f"\nAssitant: {response.output_text}")
+
+  user_input = input("\nUser: ")
+
+# user_input = input("Share anything with me and I'll translate it to French: \n")
+
+# def translate_to_french(text):
+#   llm = OpenAI()
+#   response = llm.responses.create(
+#     model="gpt-4.1-mini",
+#     temperature=1,
+#     input=f"Translate the following into French, and only include the translation itself with no introductory text: {text}"
+#   )
+
+#   return response.output_text
 
 
-print(translate_to_french(user_input))
+# print(translate_to_french(user_input))
